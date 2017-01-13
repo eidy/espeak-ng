2efd32a9779cdc915726fce16a5a817358fcd1f9
eidy <eidy@practicalcybernetics.com>
2017-01-13 22:08:16 +1000

    Try sdcard again

--- android/src/com/reecedunn/espeak/CheckVoiceData.java
+++ android/src/com/reecedunn/espeak/CheckVoiceData.java
@@ -27,8 +27,10 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
 import android.speech.tts.TextToSpeech.Engine;
 import android.util.Log;
+
 
 import com.reecedunn.espeak.SpeechSynthesis.SynthReadyCallback;
 
@@ -51,7 +53,10 @@ public class CheckVoiceData extends Activity {
     };
 
     public static File getDataPath(Context context) {
-        return new File(context.getDir("voices", MODE_PRIVATE), "espeak-data");
+
+        //return new File(context.getDir("voices", MODE_PRIVATE), "espeak-data");
+        return new File(Environment.getExternalStorageDirectory(), "espeak-data");
+
     }
 
     public static boolean hasBaseResources(Context context) {
